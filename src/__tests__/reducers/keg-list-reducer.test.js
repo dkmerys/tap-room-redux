815956b3b5ac4f283cd15aae1de6e0c37af2df36
Daniel Merys <dkmerys@gmail.com>
2020-07-10 09:04:44 -0700

    successfully pass delete keg test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,6 +2,22 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
 
+  const currentState = {
+    1: {name: 'Hayduke Helles',
+    brewery: 'Crux',
+    description: "Great beer!",
+    abv: '5.5%',
+    price: '4',
+    quantity: 118,
+    id: '1'},
+    2: {name: '3-Way IPA',
+    brewery: 'Ft. George',
+    description: "Tastes like a grapefruit.",
+    abv: '7.6%',
+    price: '6',
+    quantity: 65,
+    id: '2'}
+  }
   let action;
   const kegData = {
     name: 'Hayduke Helles',
@@ -39,6 +55,21 @@ describe('kegListReducer', () => {
         quantity: quantity,
         id: id
       }
+    });
+  });
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {name: '3-Way IPA',
+      brewery: 'Ft. George',
+      description: "Tastes like a grapefruit.",
+      abv: '7.6%',
+      price: '6',
+      quantity: 65,
+      id: '2'}
     });
   });
 });
