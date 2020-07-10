c515518b3a956d644c4cc7b3721579b5f83fd139
Daniel Merys <dkmerys@gmail.com>
2020-07-10 08:59:42 -0700

    successfully pass add keg test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,44 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'Hayduke Helles',
+    brewery: 'Crux',
+    description: "Great beer!",
+    abv: '5.5%',
+    price: '4',
+    quantity: 118,
+    id: '0'
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add a new keg to kegList', () => {
+    const { name, brewery, description, abv, price, quantity, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brewery: brewery,
+      description: description,
+      abv: abv,
+      price: price,
+      quantity: quantity,
+      id: id
+    }
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brewery: brewery,
+        description: description,
+        abv: abv,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
-export default (state = {}. action) => {
-  return state;
+export default (state = {}, action) => {
+  const { name, brewery, description, abv, price, quantity, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brewery: brewery,
+          description: description,
+          abv: abv,
+          price: price,
+          quantity: quantity,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
